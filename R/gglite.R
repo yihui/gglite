da2b889a0c7fc718967d3e934c925996f640a18f
Yihui Xie <xie@yihui.name>
2026-04-01 09:25:50 -0500

    pin to a specific version (since these patches are still ongoing)

--- R/gglite.R
+++ R/gglite.R
@@ -18,7 +18,7 @@ g2_cdn = function() {
   getOption('gglite.g2_cdn', 'https://unpkg.com/@antv/g2@5/dist/g2.min.js')
 }
 
-g2_patches_cdn = 'https://cdn.jsdelivr.net/npm/@xiee/utils/js/g2-patches.min.js'
+g2_patches_cdn = 'https://cdn.jsdelivr.net/npm/@xiee/utils@v1.14.30/js/g2-patches.min.js'
 
 #' Process a layout argument (padding, margin, or inset)
 #'
