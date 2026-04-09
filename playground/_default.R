e011d3bff1d7869a138b271718fef3a0321cacc2
Yihui Xie <xie@yihui.name>
2026-04-09 16:05:19 -0500

    ignore playground, and use formula in playground example

--- playground/_default.R
+++ playground/_default.R
@@ -1,4 +1,4 @@
 # For more info, see https://pkg.yihui.org/gglite/
 library(gglite)
-g2(penguins, bill_len ~ bill_dep, color = "species")
+g2(penguins, bill_len ~ bill_dep, color = ~ species)
 g2(penguins, bill_len ~ species)
